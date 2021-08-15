36226415c5a7e87f0fe8849ae726c85f0bb9d9b9
JuniorBaymax <cooladitya953@gmail.com>
2021-08-15 15:29:32 +0530

    Fetch API Json: Server side randering with fake API enpoints

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head';
+import Banner from '../components/Banner';
 import Header from '../components/Header';
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -11,9 +12,30 @@ export default function Home() {
           href="https://www.logo.wine/a/logo/Airbnb/Airbnb-Logo.wine.svg"
         />
       </Head>
-      {/* Header */}
       <Header />
-      {/* Banner */}
+      <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Exlpore Nearby</h2>
+          {/* pull some data from a server - from API */}
+          {exploreData?.map(item => (
+            <h1>{item.location}</h1>
+          ))}
+        </section>
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(res =>
+    res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
